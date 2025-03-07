cd65673acbaf42aa09593c2d610c17a7657f2473
Yogesh Reddy <yogiyogeshreddy1234@gmail.com>
2025-03-07 21:57:14 +0530

    added 04-arrary

---
+++ 04-arrary/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let marks=[1,2,2,3,4]
+// veggies.push("chips")//used to add at last 
+// console.log(veggies)
+// veggies.pop() //to remove the 
+// console.log(veggies)
+// console.log(veggies.toString()) //potato,apple,lithchi,banana
+// console.log(marks.toString())  //1,2,2,3,4
+// console.log(veggies.concat(marks)) //array concatination
